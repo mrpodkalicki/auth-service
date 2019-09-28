4b039362797629b5c4f535da0cf97dda165a5815
unknown <filipzynwala@gmail.com>
2019-09-28 12:08:08 +0200

    Install mongoose and connect to DB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb+srv://filipzynwala@gmail.com:coderscamp@cc-team-delta-whvgm.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Connection failed...', err));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
