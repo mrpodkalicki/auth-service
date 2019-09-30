e60c069ee4ba27e8504ce9c8e321f9b3cec30ccc
unknown <filipzynwala@gmail.com>
2019-09-30 12:16:45 +0200

    Ad updateUser handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const connectdb = require('./mongoosedb/connectDB');
 const database = require( './mongoosedb/userModel');
+const handler = require( './handlers/updateUser');
 
 const app = express();
+app.use(express.json());
 
 connectdb.connectToDB();
 
@@ -11,9 +13,13 @@ async function creatUser() {
     console.log(result)
 }   
 creatUser()
+
 app.get('/', (req, res) => {
+    console.log('Hello from GET!');
     res.send('Hello World');
 });
+
+app.put('/api/users/:id', handler.updateUser);
 
 const port = process.env.PORT || 3000;
 app.listen(port);
