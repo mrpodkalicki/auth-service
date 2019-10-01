e4f8be79aab376938444953829197cf1b94751bd
unknown <filipzynwala@gmail.com>
2019-10-01 12:36:58 +0200

    Add server-side rendered register page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const connectdb = require('./mongoosedb/connectDB');
 const database = require( './mongoosedb/userModel');
 const handler = require( './handlers/updateUser');
 
 const app = express();
+
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set('view engine', 'ejs');
 
 connectdb.connectToDB();
 
@@ -16,10 +22,18 @@ creatUser()
 
 app.get('/', (req, res) => {
     console.log('Hello from GET!');
-    res.send('Hello World');
+    res.render('register');
+    //res.send('Hello World');
 });
 
 app.put('/api/users/:id', handler.updateUser);
+
+// For testing registration page. Can be deleted if registration rout will be ready.
+app.post('/users', (req, res) => {
+    console.log('Hello from POST!');
+    res.send(req.body);
+});
+////////////////////////////////////////////////////////
 
 const port = process.env.PORT || 3000;
 app.listen(port);
