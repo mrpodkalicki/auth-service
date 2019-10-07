209eedcab95e947ffd75164bb333c7086426575f
unknown <filipzynwala@gmail.com>
2019-10-07 12:10:50 +0200

    Change default PORT for react-app to 8000

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static('public'));
 
 let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Origin', "http://localhost:8000");
     res.header('Access-Control-Allow-Methods', "PUT");
     res.header('Access-Control-Allow-Methods', "DELETE");
     res.header('Access-Control-Request-Headers', "Content-Type");
@@ -37,5 +37,5 @@ app.set('view engine', 'ejs');
 app.use('/', require('./app/routes/index'));
 app.use('/users', require('./app/routes/users'));
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 app.listen(port);
