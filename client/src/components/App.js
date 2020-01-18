4ef3c9b7a0a8330185ec08622de3da9a45290561
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-01-19 00:30:52 +0100

    created App component

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import MainView from './MainView';
+
+const App = () =>{
+    return (
+        <div>
+            <MainView/>
+        </div>
+    );
+};
+
+export default App;
