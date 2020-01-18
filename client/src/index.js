3a5aa246004ea03f1e1b0a564d0fdc4c703946fb
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-01-19 00:27:42 +0100

    User TAble component changed to App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { UserTable } from './components/components';
+
+
+import App from './components/App';
+
+
+// import { createStore, applyMiddleware } from 'redux';
+// import { UserTable } from './components/components';
 //import thunk from 'redux-thunk';
 //import rootReducer from './reducers/index';
 
 //const store = createStore(rootReducer, applyMiddleware(thunk));
 
-ReactDOM.render(<UserTable />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
