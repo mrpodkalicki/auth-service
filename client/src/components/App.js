734d42b5e327bf6fae9db1740c1e9d5be8af8192
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-01-19 00:42:27 +0100

    added Route for MainView

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import { BrowserRouter,Route } from 'react-router-dom';
 
 import MainView from './MainView';
 
 const App = () =>{
     return (
         <div>
+            <BrowserRouter>
+                <Route path="/"  exact components = {MainView}  />
+            </BrowserRouter>
             <MainView/>
         </div>
     );
