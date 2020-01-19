3618a08b6670ba9d66c01d61f8d1f4642e4aeebf
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-01-19 23:48:13 +0100

    has been installed  Material- UI library , added meta tag to  RWD

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter,Route } from 'react-router-dom';
 
+
 import MainView from './MainView';
+import NavigationBar from './NavigationBar';
 
 const App = () =>{
     return (
         <div>
+            <NavigationBar/>
             <BrowserRouter>
                 <Route path="/"  exact components = {MainView}  />
             </BrowserRouter>
---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class NavigationBar extends React.Component {
+    render(){
+        return (
+            <div>
+                NavBAR
+            </div>
+        );
+    }
+        
+};
+
+export default NavigationBar;
