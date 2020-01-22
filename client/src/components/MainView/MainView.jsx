d3e011608634580484d130bc2e61753d08040089
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-01-22 12:40:52 +0100

    have been created three Image objects , one Images object

--- client/src/components/MainView/MainView.jsx
+++ client/src/components/MainView/MainView.jsx
@@ -5,13 +5,10 @@ import perosnPath from './Header/images/person.png';
 import trainglePath from './Header/images/triangle.png';
 
 
-
 const imgOne = new Image('arrow', arrowPath, 'curved arrow');
+const imgTwo = new Image('person', perosnPath, 'outline of person ');
+const imgThree = new Image('triangle', trainglePath, 'shape of triangle')
 
-const images = new Images()
-images.addImgObj(imgOne)
-images.addImgObj(imgOne)
-
-
+const images= Images.addImgObj(imgOne, imgTwo, imgThree );
 
 export {images}
