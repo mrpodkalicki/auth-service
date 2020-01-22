9ae56f12a6ed41da8877ad4c03ebf8fc0ee70106
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2020-01-22 16:27:24 +0100

    has been added new object of image, added imagebox and style CSS to it

--- client/src/components/MainView/Header/Header.js
+++ client/src/components/MainView/Header/Header.js
@@ -3,11 +3,13 @@ import propTypes from 'prop-types';
 
 import {withStyles} from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 
 import {images} from '../MainView.jsx'
 
+const {arrow, person, triangle, trucksHeader} = images;
 
 const styles = (theme) => ({
     color_box:{
@@ -16,7 +18,8 @@ const styles = (theme) => ({
         display:"flex",
         flexDirection:"column",
         padding:"0.2rem 5rem",
-        position:"relative"
+        position:"relative",
+        // width:"100%"
         
     },
 
@@ -66,8 +69,36 @@ const styles = (theme) => ({
         position:"absolute",
         bottom:"1rem",
         right:"0.8rem"
-    }
+    },
 
+    
+    image_box:{
+        display:"flex",
+        justifyContent:"flex-end",
+        position:"relative"
+
+    },
+
+    
+    image_box__text:{
+        fontSize:"5rem",
+        // position:"absolute",
+        right:"0px",
+        zIndex:"2",
+        fontFamily:"Rye",
+        color:"white"
+    },
+
+    image_box__img:{
+        backgroundImage:`${trucksHeader.src}`,
+        width:"100%",
+        height:"33rem",
+        border:"solid red",
+        // zIndex:"1",
+        // minWidth:"120rem",
+        position:"absolute"
+
+    },
 })
 
 class Header extends React.Component {
@@ -81,30 +112,38 @@ class Header extends React.Component {
     
 
     render(){
-        const {arrow, person, triangle} = images;
+        
         
         const {classes} = this.props;
         return(
-            <Box className={classes.color_box} justifyContent="center"  alignItems="center">
-                <Typography  variant ="h2"   className = {classes.color_box___text}>
-                    Do you want in a simple and fast way to manage your resources?
-                </Typography>
-                <Box className = { classes.color_box__regis_cont }>
-                    <Box className = { classes.color_box__regis_box }>
-                        <Typography variant = "body1" className = { classes.color_box___text___smaller }>
-                            REGISTER
-                        </Typography>
-                        <img src = { arrow.src } alt = { arrow.alt }   className = { classes.color_box__arrow }></img>
-                        <Link href="#"  onClick={this.preventDefault} >
-                            <img  src = { person.src } alt = { person.alt }  className = {classes.color_box__person_icon}></img>
-                        <img  src = { triangle.src } alt = { triangle.alt } className = {classes.color_box__triangle}></img>
-                        </Link>
-                        
+            <Box >
+                <Box className={classes.color_box} justifyContent="center"  alignItems="center">
+                    <Typography  variant ="h2"   className = {classes.color_box___text}>
+                        Do you want in a simple and fast way to manage your resources?
+                    </Typography>
+                    <Box className = { classes.color_box__regis_cont }>
+                        <Box className = { classes.color_box__regis_box }>
+                            <Typography variant = "body1" className = { classes.color_box___text___smaller }>
+                                REGISTER
+                            </Typography>
+                            <img src = { arrow.src } alt = { arrow.alt }   className = { classes.color_box__arrow }></img>
+                            <Link href="#"  onClick={this.preventDefault} >
+                                <img  src = { person.src } alt = { person.alt }  className = {classes.color_box__person_icon}></img>
+                            <img  src = { triangle.src } alt = { triangle.alt } className = {classes.color_box__triangle}></img>
+                            </Link>
+                        </Box>
                     </Box>
-                    
                 </Box>
-               
+                <Box className = { classes.image_box }> 
+                    <Typography className = { classes.image_box__text }>
+                        Register and check for free !!!
+                    </Typography>
+                    <Box src = { trucksHeader.src } alt = { trucksHeader.alt } className = { classes.image_box__img }></Box>
+                </Box>
             </Box>
+            
+            
+               
         )
     }
 }
--- client/src/components/MainView/MainView.jsx
+++ client/src/components/MainView/MainView.jsx
@@ -3,12 +3,14 @@ import {Image, Images} from '../App.jsx';
 import arrowPath from './Header/images/arrow.png';
 import perosnPath from './Header/images/person.png';
 import trainglePath from './Header/images/triangle.png';
+import truckHeaderPath from './Header/images/trucksHeader.png'
 
 
 const imgOne = new Image('arrow', arrowPath, 'curved arrow');
 const imgTwo = new Image('person', perosnPath, 'outline of person ');
 const imgThree = new Image('triangle', trainglePath, 'shape of triangle')
+const imgFour = new Image('trucksHeader',truckHeaderPath, 'trucks stand in row on parking' )
 
-const images= Images.addImgObj(imgOne, imgTwo, imgThree );
+const images= Images.addImgObj(imgOne, imgTwo, imgThree, imgFour );
 
 export {images}
